49160bdc0cb442f01b8b62a46d61e058357d2cb1
Sean Eagan <seaneagan1@gmail.com>
2015-02-16 16:17:50 -0600

    Add den spec demo

--- CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-
-# 0.1.3
-
-* Added `den bump` command.
--- bin/den.dart
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env dart
-
-import 'package:den/src/den.dart';
-import 'package:unscripted/unscripted.dart';
-
-main(arguments) => declare(Den).execute(arguments);
