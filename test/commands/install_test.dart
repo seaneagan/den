53d5d13d879434caf54ae1e44b78081517001c40
Damon Douglas <douglas.damon@gmail.com>
2015-04-14 20:11:00 -0400

    check package tests and install command test

--- test/commands/install_test.dart
+++ test/commands/install_test.dart
@@ -11,23 +11,61 @@ main() {
   var installCommandTest = new InstallCommandTest();
   Directory.current = installCommandTest.workingDir;
   List packages;
-  group('install', (){
+  group('install', () {
     setUp(() => installCommandTest.loadPackages());
-    test('setup ok', (){
+    test('setup ok', () {
       packages = installCommandTest.packages;
-      expect(packages,isNotNull);
-      expect(packages,isNotEmpty);
+      expect(packages, isNotNull);
+      expect(packages, isNotEmpty);
     });
-    group('<name>', (){
-      group('single', (){
-        test('', ()=> installCommandTest.run([packages.first.name]));
+    group('<name>', () {
+      test('', () {
+        Future.forEach([
+          installCommandTest.run([packages[0].name]),
+          installCommandTest.run([packages[0].name, packages[1].name]),
+          installCommandTest.run([
+            packages[0].name, packages[1].name, packages[2].name
+          ]),
+          installCommandTest.run([
+            packages[0].name,
+            packages[1].name,
+            packages[2].name,
+            packages[3].name
+          ]),
+          installCommandTest.run([
+            packages[0].name,
+            packages[1].name,
+            packages[2].name,
+            packages[3].name,
+            packages[4].name
+          ]),
+          installCommandTest.run([packages[0].name], dev: true),
+          installCommandTest.run([
+            packages[0].name, packages[1].name],
+            dev: true),
+          installCommandTest.run([
+            packages[0].name, packages[1].name, packages[2].name
+          ], dev: true),
+          installCommandTest.run([
+            packages[0].name,
+            packages[1].name,
+            packages[2].name,
+            packages[3].name
+          ], dev: true),
+          installCommandTest.run([
+            packages[0].name,
+            packages[1].name,
+            packages[2].name,
+            packages[3].name,
+            packages[4].name
+          ], dev: true)
+        ], (e) {});
       });
     });
   });
 }
 
 class InstallCommandTest {
-
   String _pubspec = "name: foo";
   String get workingDirPath => p.dirname(Platform.script.path);
   Directory get workingDir => new Directory(workingDirPath);
@@ -38,45 +76,45 @@ class InstallCommandTest {
 
   List packages;
 
-
-  Future loadPackages() => new Future((){
+  Future loadPackages() => new Future(() {
     var baseUrl = "https://pub.dartlang.org/packages/";
     var randomPackageList = new List.from(packageList);
     randomPackageList.shuffle();
-    var urlList = randomPackageList.sublist(0,5).map((name) => "$baseUrl$name.json");
-    return Future.wait(
-      urlList.map((url) => fetchPackage(url))
-    ).then((_packages){
+    var urlList =
+        randomPackageList.sublist(0, 5).map((name) => "$baseUrl$name.json");
+    return Future
+        .wait(urlList.map((url) => fetchPackage(url)))
+        .then((_packages) {
       packages = _packages;
       return packages;
     });
   });
 
-  Future run(List packageListArgs, {String source: 'hosted', bool dev: false}) => new Future((){
-    // var _run = new Completer();
-    return pubspec.writeAsString(_pubspec)
-    .then((_){
-      StreamSubscription subscription;
-      subscription = pubspec.watch().listen((e){
-        var doc = loadYaml(pubspec.readAsStringSync());
-        var dependencies = doc['dependencies'];
-        if(source=='hosted') {
-          packageListArgs.forEach((arg){
-            if(arg.contains('#')) {
-              expect(1,1);
-            } else {
-              expect(dependencies.containsKey(arg), isTrue);
-            }
-          });
-        }
-        else {
-          expect(1,1);
-        }
-        return subscription.cancel();
-      });
+  Future run(
+    List packageListArgs,
+    {
+      String source: 'hosted', bool dev: false}
+    ) => new Future(() {
+      return pubspec.writeAsString(_pubspec).then((_) {
+        StreamSubscription subscription;
+        subscription = pubspec.watch().listen((e) {
+          var doc = loadYaml(pubspec.readAsStringSync());
+          var dependencies = doc['dependencies'];
+          if (source == 'hosted') {
+            packageListArgs.forEach((arg) {
+              if (arg.contains('#')) {
+                expect(1, 1);
+              } else {
+                expect(dependencies.containsKey(arg), isTrue);
+              }
+            });
+          } else {
+            expect(1, 1);
+          }
+          return subscription.cancel();
+        });
 
       command.install(packageListArgs);
     });
-    // return _run.future;
   });
 }
--- test/common.dart
+++ test/common.dart
@@ -2,20 +2,20 @@ library den.test.common;
 
 import 'dart:math';
 
-String randomString(int length, {String allow: "ABCDEFGHIJKLMNSTOPQRSTUVWXYZabcdefghijklmnstopqrstuvwxyz"}) {
+String randomString(int length,
+    {String allow: "ABCDEFGHIJKLMNSTOPQRSTUVWXYZabcdefghijklmnstopqrstuvwxyz"}) {
   var rand = new Random();
   var candidates = allow.split('');
   return new List.generate(
-    length,
-    (index) => candidates[rand.nextInt(candidates.length)]
-  ).join("");
+      length, (index) => candidates[rand.nextInt(candidates.length)]).join("");
 }
 
 String randomAlpha(int length) => randomString(length);
 
-String randomAlphaNumeric(int length) => randomString(length, allow: "0123456789ABCDEFGHIJKLMNSTOPQRSTUVWXYZabcdefghijklmnstopqrstuvwxyz");
+String randomAlphaNumeric(int length) => randomString(length,
+    allow: "0123456789ABCDEFGHIJKLMNSTOPQRSTUVWXYZabcdefghijklmnstopqrstuvwxyz");
 
 String randomNonAlphaNumeric(int length) {
   var allow = "`~!@#\$%^&*()-+={}[]|<>,.:;";
-  return randomString(length, allow:allow);
+  return randomString(length, allow: allow);
 }
